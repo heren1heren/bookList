3a64702b516626d1a68be0ef774e0c627b087a21
heren <heren1heren@gmail.com>
2023-12-13 09:06:43 -0700

    nothing

--- src/UI.js
+++ src/UI.js
@@ -25,8 +25,36 @@ export default class UI {
     <td>${book.author}</td>
     <td>${book.isbn}</td>
     <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-    
+     
     `;
     list.appendChild(row);
+  }
+
+  static clearField(form) {
+    form.querySelector('#title').value = '';
+    form.querySelector('#author').value = '';
+    form.querySelector('#isbn').value = '';
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`; // using booststrap ...
+    const messageElement = document.createTextNode(message);
+    div.appendChild(messageElement);
+
+    const container = document.querySelector('.container');
+    const existingDiv = container.querySelector('.alert');
+
+    if (existingDiv) container.removeChild(existingDiv);
+    const form = document.querySelector('#book-form'); // for insert method
+    container.insertBefore(div, form);
+  }
+
+  static deleteBook(element) {
+    if (element.classList.contains('delete')) {
+      // delete the whole parent
+      // what the heck is .remove() and parentElement -> so convenient
+      element.parentElement.parentElement.remove();
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
+/* eslint-disable import/extensions */
 import Book from './book.js'; // using export default so we can't have {} around the import variables
 import UI from './UI.js'; // UI is a class from other file
 import './style.css';
 import './bootstrap.css';
-// book class: represents a book
 
-// UI class: Handle UI Tasks
+const bookForm = document.querySelector('#book-form');
+
 // Store Class: Handles Storage
 
 // Event: display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 // Event: Add a Book
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = bookForm.querySelector('#title').value;
+  const author = bookForm.querySelector('#author').value;
 
+  const isbn = +bookForm.querySelector('#isbn').value;
+  if (Number.isNaN(isbn)) {
+    UI.showAlert('isbn must be a number', 'danger');
+  } else { // using if else to check validity
+    const book = new Book(title, author, isbn);
+    UI.clearField(bookForm);
+  }
+});
+// validating Form
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+});
